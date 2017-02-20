2efcbf0f7c8650ee6c2a260e675285d4ffb6b5aa
星川かえで <futursolo@icloud.com>
2017-02-20 16:42:58 +0800

    Implement FrozenMagicDict.

--- magicdict/__init__.py
+++ magicdict/__init__.py
@@ -69,15 +69,11 @@ class _MagicKeysView(KeysView[_K], Generic[_K], _TotalOrdering):
         return len(self._map)
 
     def __iter__(self) -> Iterator[_K]:
-        with self._map._lock:
-            pairs = self._map._kv_pairs.values()
-
-        for key, _ in pairs:
+        for key, _ in self._map._kv_pairs.values():
             yield key
 
     def __contains__(self, key: Any) -> bool:
-        with self._map._lock:
-            return key in self._map._pair_ids.keys()
+        return key in self._map._pair_ids.keys()
 
     def __and__(self, obj: Iterable[Any]) -> Set[Any]:
         return set(self) & set(obj)
@@ -103,17 +99,13 @@ class _MagicValuesView(ValuesView[_V], Generic[_V]):
         return len(self._map)
 
     def __iter__(self) -> Iterator[_V]:
-        with self._map._lock:
-            pairs = self._map._kv_pairs.values()
+        pairs = self._map._kv_pairs.values()
 
         for _, value in pairs:
             yield value
 
     def __contains__(self, value: Any) -> bool:
-        with self._map._lock:
-            pairs = self._map._kv_pairs.values()
-
-        for _, _value in pairs:
+        for _, _value in self._map._kv_pairs.values():
             if _value == value:
                 return True
 
@@ -140,15 +132,11 @@ class _MagicItemsView(ItemsView[_K, _V], Generic[_K, _V], _TotalOrdering):
         return len(self._map)
 
     def __iter__(self) -> Iterator[Tuple[_K, _V]]:
-        with self._map._lock:
-            pairs = self._map._kv_pairs.values()
-
-        for key, value in pairs:
+        for key, value in self._map._kv_pairs.values():
             yield (key, value)
 
     def __contains__(self, pair: Any) -> bool:
-        with self._map._lock:
-            return pair in self._map._kv_pairs.values()
+        return pair in self._map._kv_pairs.values()
 
     def __and__(self, obj: Iterable[Any]) -> Set[Any]:
         return set(self) & set(obj)
@@ -173,61 +161,124 @@ class FrozenMagicDict(Mapping[_K, _V], Generic[_K, _V]):
             "collections.OrderedDict[_Identifier, Tuple[_K, _V]]" = \
             collections.OrderedDict()
 
-        self._lock = threading.Lock()
+        def add_one(key: _K, value: _V) -> None:
+            identifier = _Identifier()
+
+            if key not in self.keys():
+                self._pair_ids[key] = [identifier]
+
+            else:
+                self._pair_ids[key].append(identifier)
+
+            self._kv_pairs[identifier] = (key, value)
+
+        if len(args):
+            if len(args) > 1:
+                raise TypeError(
+                    ("update expected at most 1 positional argument, "
+                     "got {} args.").format(len(args)))
+
+            else:
+                if isinstance(args[0], collections.abc.Mapping):
+                    for k, v in args[0].items():
+                        add_one(k, v)
+
+                elif isinstance(args[0], collections.abc.Iterable):
+                    for k, v in args[0]:
+                        add_one(k, v)
+
+                else:
+                    raise TypeError(
+                        ("update expected a Mapping or an Iterable "
+                         "as the positional argument, got {}.")
+                        .format(type(args[0])))
+
+        for k, v in kwargs.items():
+            add_one(k, v)
 
     def __getitem__(self, key: _K) -> _V:
-        raise NotImplementedError
+        identifier = self._pair_ids[key][0]
+        _, value = self._kv_pairs[identifier]
+        return value
 
     def __iter__(self) -> Iterator[_K]:
-        raise NotImplementedError
+        return iter(self.keys())
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        return len(self._kv_pairs)
 
     def __contains__(self, key: Any) -> bool:
-        raise NotImplementedError
+        return key in self._pair_ids
 
     def __eq__(self, obj: Any) -> bool:
-        raise NotImplementedError
+        if isinstance(obj, collections.abc.Mapping):
+            return self.items() == obj.items()
+
+        if isinstance(obj, collections.abc.Iterable):
+            return self.items() == obj
+
+        return False
 
     def __ne__(self, obj: Any) -> bool:
-        raise NotImplementedError
+        return not self.__eq__(obj)
 
     def __str__(self) -> str:
-        raise NotImplementedError
+        return "{}({})".format(
+            self.__class__.__name__,
+            repr([(key, value) for (key, value) in self.items()]))
 
-    def __reversed__(self) -> FrozenMagicDict[_K, _V]:
-        raise NotImplementedError
+    def __reversed__(self) -> "FrozenMagicDict[_K, _V]":
+        return self.__class__(reversed(list(self._kv_pairs.values())))
 
-    def get_first(self, key: _K, default: _V=None) -> Optional[_V]:
-        raise NotImplementedError
+    def get_first(self, key: _K, default: Optional[_V]=None) -> Optional[_V]:
+        if key not in self.keys():
+            if default is _DEFAULT_MARK:
+                raise KeyError(key)
 
-    def get_last(self, key: _K, default: _V=None) -> Optional[_V]:
-        raise NotImplementedError
+            else:
+                return default
 
-    def get_iter(self, key: _K, default: _V=None) -> Optional[_V]:
-        raise NotImplementedError
+        return self[key]
+
+    def get_last(self, key: _K, default: Optional[_V]=None) -> Optional[_V]:
+        if key not in self.keys():
+            if default is _DEFAULT_MARK:
+                raise KeyError(key)
+
+            else:
+                return default
+
+        identifier = self._pair_ids[key][-1]
+        _, value = self._kv_pairs[identifier]
+        return value
+
+    def get_iter(self, key: _K) -> Iterator[_V]:
+        for identifier in self._pair_ids.get(key, []):
+            _, value = self._kv_pairs[identifier]
+
+            yield value
 
     def get_list(self, key: _K) -> List[_V]:
-        raise NotImplementedError
+        return list(self.get_iter(key))
 
     def copy(self) -> "FrozenMagicDict[_K, _V]":
-        raise NotImplementedError
+        return self.__class__(self)
 
     def keys(self) -> KeysView[_K]:
-        raise NotImplementedError
+        return _MagicKeysView(self)
 
     def values(self) -> ValuesView[_V]:
-        raise NotImplementedError
+        return _MagicValuesView(self)
 
     def items(self) -> ItemsView[_K, _V]:
-        raise NotImplementedError
+        return _MagicItemsView(self)
 
     get = get_first
     __repr__ = __str__
 
 
-class MagicDict(MutableMapping[_K, _V], Generic[_K, _V]):
+class MagicDict(
+        MutableMapping[_K, _V], FrozenMagicDict[_K, _V], Generic[_K, _V]):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self._pair_ids: Dict[_K, List[_Identifier]] = {}
         self._kv_pairs: \
@@ -260,45 +311,9 @@ class MagicDict(MutableMapping[_K, _V], Generic[_K, _V]):
             for identifier in ids:
                 del self._kv_pairs[identifier]
 
-    def __iter__(self) -> Iterator[_K]:
-        return iter(self.keys())
-
-    def __len__(self) -> int:
-        return len(self._kv_pairs)
-
-    def __contains__(self, key: Any) -> bool:
-        return key in self._pair_ids
-
-    def __eq__(self, obj: Any) -> bool:
-        if isinstance(obj, collections.abc.Mapping):
-            return self.items() == obj.items()
-
-        if isinstance(obj, collections.abc.Iterable):
-            return self.items() == obj
-
-        return False
-
-    def __ne__(self, obj: Any) -> bool:
-        return not self.__eq__(obj)
-
-    def __str__(self) -> str:
-        return "{}({})".format(
-            self.__class__.__name__,
-            repr([(key, value) for (key, value) in self.items()]))
-
     def __reversed__(self) -> "MagicDict[_K, _V]":
         with self._lock:
             return self.__class__(reversed(list(self._kv_pairs.values())))
-
-    def get_first(self, key: _K, default: Optional[_V]=None) -> Optional[_V]:
-        if key not in self.keys():
-            if default is _DEFAULT_MARK:
-                raise KeyError(key)
-
-            else:
-                return default
-
-        return self[key]
 
     def get_last(self, key: _K, default: Optional[_V]=None) -> Optional[_V]:
         if key not in self.keys():
@@ -321,9 +336,6 @@ class MagicDict(MutableMapping[_K, _V], Generic[_K, _V]):
 
         for val in vals:
             yield val
-
-    def get_list(self, key: _K) -> List[_V]:
-        return list(self.get_iter(key))
 
     def add(self, key: _K, value: _V) -> None:
         if key in self.keys():
@@ -416,15 +428,3 @@ class MagicDict(MutableMapping[_K, _V], Generic[_K, _V]):
 
     def copy(self) -> "MagicDict[_K, _V]":
         return self.__class__(self)
-
-    def keys(self) -> KeysView[_K]:
-        return _MagicKeysView(self)
-
-    def values(self) -> ValuesView[_V]:
-        return _MagicValuesView(self)
-
-    def items(self) -> ItemsView[_K, _V]:
-        return _MagicItemsView(self)
-
-    get = get_first
-    __repr__ = __str__
